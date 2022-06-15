4fb097a9b5f91eb24312e5df81e574a52caf5d7f
EVOKE <evoke.lektrique@gmail.com>
2022-06-16 01:06:46 +0430

    Update request method, Add tests

---
+++ spec/api_spec.lua
@@ -0,0 +1,33 @@
+describe("Unit tests for Zarinpal library", function()
+
+	-- Import the module
+	zarinpal = require("zarinpal")
+
+	-- Required options for a transaction request
+	data = {
+		-- Money amount
+		amount = 20000,
+		-- Your secret 36 character merchant code
+		merchant_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+		-- Description for the transaction
+		description = "Test description",
+		-- Redirect url
+		callack_url = 'http://localhost:8000/',
+		-- Customer phone number
+		mobile = "09000000000",
+		-- Customer email address
+		email = "customer_email@example.com",
+	}
+
+	-- Initializing the module, and there are two ways to instance a `zarinpal`,
+	--
+	-- `x = zarinpal(your_data_table)`
+	-- `y = zarinpal.new(your_data_table)`
+	--
+	request = zarinpal.new()
+	-- Gateway mode(Optional) default is set to "api"
+	request.sandbox = true
+	request_result = zarinpal:request(data)
+
+	print("Result:", request_result)
+end)
--- src/zarinpal.lua
+++ src/zarinpal.lua
@@ -1,26 +1,56 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+local json = require("json")
+
 local zarinpal = {
-  _metatable = "awfawf",
   api = {
     version = "v4",
     endpoints = {
       request = "/payment/request.json",
       verify = "/payment/verify.json",
     },
-  },
+  }
 }
 
 zarinpal.__index = zarinpal
 
-zarinpal.new = function(configuration)
-  return setmetatable(configuration, zarinpal)
+zarinpal.__newindex = function(_, key, value)
+  zarinpal[key] = value
 end
 
-zarinpal.request = function(self)
+zarinpal.new = function()
+  return setmetatable({}, zarinpal)
+end
+
+zarinpal.request = function(self, data)
   local url = self:generate_zarinpal_url() ..
               self.api.version ..
               self.api.endpoints.request
 
-  return url
+  local request_data = json.encode(data)
+  local response = {}
+  -- -- Send a http request to "Zarinpal" is endpoint and receive its data
+  local result, response_code, response_headers, response_status = http.request({
+    method = "POST",
+    source = ltn12.source.string(request_data),
+    url = url,
+    headers = {
+      ["content-type"] = "application/json",
+      ["content-length"] = tostring(#request_data),
+    },
+    sink = ltn12.sink.table(response)
+  })
+
+  -- Convert the received request body data into string
+  response = table.concat(response)
+
+  -- Print the result as an error if the response header status is other than "OK/200"
+  --
+  -- TODO: Maybe display a meaningful error?
+  --
+  if response_code ~= 200 then error(response) end
+
+  return json.decode(response)
 end
 
 zarinpal.verify = function(self)
